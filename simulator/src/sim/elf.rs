23ebd9ad5120638a7fb5d219b3fa7578fda6710d
Haouo <q18131500@gs.ncku.edu.tw>
2025-05-29 00:56:18 +0800

    add some codes for ELF parsing and loading

--- simulator/src/sim/elf.rs
+++ simulator/src/sim/elf.rs
@@ -1,19 +1,42 @@
 use std::error::Error;
 use std::fs;
 
-use object;
+use object::{Object, ObjectSection, ObjectSegment};
 
-pub fn elf_loader(file: &String) -> Result<Vec<u8>, Box<dyn Error>> {
-    let object_fs_read = fs::read(file)?;
-    let object_parse = object::File::parse(&*object_fs_read)?;
-    todo!();
-}
+pub fn elf_loader(file_path: &String) -> Result<Vec<u8>, Box<dyn Error>> {
+    let file = fs::read(file_path)?;
+    let object_parse = object::File::parse(file.as_slice())?;
 
-mod unit_tests {
-    use super::*;
+    // Check ELF format
+    match object_parse {
+        object::File::Elf32(_) => {}
+        _ => {
+            panic!("Invalid ELF format!");
+        }
+    }
+    // check ELF architecture
+    match object_parse.architecture() {
+        object::Architecture::Riscv32 => {}
+        _ => {
+            panic!("Invalid architecture for the ELF file!");
+        }
+    }
+    // check the kind of the ELF
+    match object_parse.kind() {
+        object::ObjectKind::Executable => {}
+        _ => {
+            panic!("Invalid kind of the ELF file!");
+        }
+    }
+    // check endianness (only little is allowed)
+    assert!(object_parse.is_little_endian());
 
-    #[test]
-    fn read_example_object_file() {
+    // load sections into Vec<u8>
+    let ret: Vec<u8> = Vec::with_capacity(0x40000); // 128-KiB Memory
+    for seg in object_parse.segments() {
+        assert!(seg.address() + seg.size() < 0x40000); // memory boundary check
+        let seg_data = seg.data().unwrap();
         todo!();
     }
+    Ok(ret)
 }
