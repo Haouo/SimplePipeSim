a7ebcf28899a4ed7c3a6e03e9d5a20e9b7b4ffe0
Haouo <q18131500@gs.ncku.edu.tw>
2025-05-29 18:09:29 +0800

    Complete ELF Loader

--- simulator/src/sim/elf.rs
+++ simulator/src/sim/elf.rs
@@ -1,42 +1,43 @@
-use std::error::Error;
-use std::fs;
+use object::{Object, ObjectSegment};
 
-use object::{Object, ObjectSection, ObjectSegment};
+const MEM_SIZE: usize = 0x40000;
 
-pub fn elf_loader(file_path: &String) -> Result<Vec<u8>, Box<dyn Error>> {
-    let file = fs::read(file_path)?;
-    let object_parse = object::File::parse(file.as_slice())?;
+pub fn elf_loader(file_path: &String) -> Vec<u8> {
+    let file = std::fs::read(file_path).expect("Cannot read file!");
+    let object_parse =
+        object::File::parse(file.as_slice()).expect("Cannot parse the given ELF file!");
 
     // Check ELF format
     match object_parse {
         object::File::Elf32(_) => {}
-        _ => {
-            panic!("Invalid ELF format!");
-        }
+        _ => panic!("The given ELF file is not EFL32 format!"),
     }
     // check ELF architecture
     match object_parse.architecture() {
         object::Architecture::Riscv32 => {}
-        _ => {
-            panic!("Invalid architecture for the ELF file!");
-        }
+        _ => panic!("Invalid architecture for the ELF file! (only RISCV32 is allowed)"),
     }
     // check the kind of the ELF
     match object_parse.kind() {
         object::ObjectKind::Executable => {}
-        _ => {
-            panic!("Invalid kind of the ELF file!");
-        }
+        _ => panic!("Invalid kind of the ELF file! (only executable is allowed)"),
     }
     // check endianness (only little is allowed)
-    assert!(object_parse.is_little_endian());
+    assert!(
+        object_parse.is_little_endian(),
+        "The given ELF file is not little-endian!"
+    );
 
     // load sections into Vec<u8>
-    let ret: Vec<u8> = Vec::with_capacity(0x40000); // 128-KiB Memory
+    let mut ret: Vec<u8> = vec![0u8; MEM_SIZE]; // 128-KiB Memory
     for seg in object_parse.segments() {
-        assert!(seg.address() + seg.size() < 0x40000); // memory boundary check
-        let seg_data = seg.data().unwrap();
-        todo!();
+        assert!(seg.address() + seg.size() < MEM_SIZE as u64); // memory boundary check
+        let start_addr = seg.address() as usize;
+        let end_addr = (seg.address() + seg.size()) as usize;
+        ret[start_addr..end_addr].copy_from_slice(
+            seg.data()
+                .expect("Data of loadable segment should not be None!"),
+        );
     }
-    Ok(ret)
+    ret
 }
