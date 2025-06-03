95a8ffbd84f1dfce1a5e7504083fb242de5a49e3
Haouo <q18131500@gs.ncku.edu.tw>
2025-06-04 02:53:13 +0800

    Add new RISC-V test program: factorial

--- runtime/bin/factorial.rs
+++ runtime/bin/factorial.rs
@@ -1,1 +1,22 @@
+#![no_std]
+#![no_main]
 
+extern crate alloc;
+use core::fmt::Write;
+
+use runtime;
+use runtime::basic_io::Stdout;
+
+fn factorial(n: u32) -> u32 {
+    if n == 0 || n == 1 {
+        n
+    } else {
+        n * factorial(n - 1)
+    }
+}
+
+#[no_mangle]
+fn main() -> core::fmt::Result {
+    writeln!(Stdout, "The result of factorial(20) is {}", factorial(20))?;
+    Ok(())
+}
